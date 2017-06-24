4ee1db7fdbd6c325eaff722ce33e7450f5421d5c
allan Ruka <alan.ruka@gmail.com>
2017-06-24 11:07:32 -0300

    Update upload.controller.spec.js
    add test

--- app/upload.controller.spec.js
+++ app/upload.controller.spec.js
@@ -12,4 +12,23 @@ describe('controllers', function(){
   it('should have defined uploadController', function() {
     expect(ctrl).toBeDefined();
   });
+  
+  beforeEach(inject(function($controller, _$httpBackend_) {
+    $scope = {};
+    var controller = $controller('uploadFileCtrl', { $scope: $scope });
+    var URL = 'https://api.wistia.com/v1/medias/.json?api_password=2b3accaa5d4c7086bb86fcbf9f6e3f27f5e04160e124e97ce223c3ef3aedb247'
+    $httpBackend = _$httpBackend_;
+
+    //Tell the $httpBackend to respond with our mockLangularList array. Or whatever the api actually returns.Array used for example.
+    var mockMedia = [{key: ''},{key: ''},{key: ''}];
+    $httpBackend.whenGET(URL).respond(200, mockMedia);
+
+ }));
+
+  it('should load default media', function()
+  {
+      $scope.checkStatus(); // <-- Make call
+      $httpBackend.flush();
+      expect($scope.checkStatus).not.toEqual(undefined);
+  });
 });
